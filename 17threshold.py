c470d9d47239b92597bd0115de608441aa4fbca9
RationalStudent <shafqatameen316@gmail.com>
2025-03-04 06:35:36 +0530

    learned the threshold

--- 17threshold.py
+++ 17threshold.py
@@ -1,5 +1,19 @@
 import cv2 as cv
 
 img = cv.imread('images/park.jpg')
-cv.imshow('Park', img)
+gray = cv.cvtColor(img, cv.COLOR_BGR2GRAY)
 
+# Simple threshold
+threshold , thresh = cv.threshold(gray, 150,255,cv.THRESH_BINARY)
+cv.imshow('Simple Threshold', thresh)
+
+# Inverse threshold
+threshold,thresh= cv.threshold(gray,150,255,cv.THRESH_BINARY_INV)
+cv.imshow('Inverse Threshold', thresh)
+
+
+# Adaptive threshold
+adp_thresh=cv.adaptiveThreshold(gray,255,cv.ADAPTIVE_THRESH_MEAN_C,cv.THRESH_BINARY,11,1)
+cv.imshow('Adaptive Threshold', adp_thresh)
+
+cv.waitKey(0)
