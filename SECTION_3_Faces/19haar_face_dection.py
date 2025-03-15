4ae212dfd32797d1c8aaa810e4af8b6dc4e5687b
RationalStudent <shafqatameen316@gmail.com>
2025-03-16 00:18:56 +0530

    able to get the face coordinates and abel to draw the box on the face

---
+++ SECTION_3_Faces/19haar_face_dection.py
@@ -0,0 +1,18 @@
+import cv2 as cv
+#load and read the image
+img=cv.imread("images/lady.jpg")
+cv.imshow("lady",img)
+#conver into gray
+gray_img=cv.cvtColor(img,cv.COLOR_BGR2GRAY)
+cv.imshow("gray",gray_img)
+#haar cascade
+haar_cascade=cv.CascadeClassifier("SECTION_3_Faces/haar_face.xml")
+
+#faces_rect=haar_cascade.detectMultiScale(gray_img,scaleFactor=1.1,minNeighbors=4)
+faces_rect=haar_cascade.detectMultiScale(gray_img,1.1,4)
+#dectect the face
+for x,y,w,h in faces_rect:
+    cv.rectangle(img,(x,y),(x+w,y+h),(0,255,0),3)
+cv.imshow("face",img)
+print(f'number of faces found={len(faces_rect)}')
+cv.waitKey(0)
